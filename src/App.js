b201b410ab607cf321819ad2bc830d6f10b12074
Maksudbek <Maksudergashev07@gmail.com>
2022-06-06 17:11:27 +0500

    Created Project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Employee } from "./components/employee";
+import "./sass/main.scss"
+
+function App() {
+  return (
+    <>
+    <Employee/>
+    </>
+  );
+}
+
+export default App;
